4e6b1ee9c81d22d5fe87417a7173abe272f170ba
S.Karen <karen.sayadyan62@gmail.com>
2025-09-29 16:25:43 +0400

    now both are the same name

--- index1.js
+++ index1.js
@@ -2,6 +2,6 @@ console.log('Executed index 1')
 console.log(process.env.PREVIEW_LAST_COMMIT_HASH)
 console.log(process.env.PREVIEW_LAST_COMMIT_MESSAGE)
 console.log(process.env.PREVIEW_LAST_COMMIT_DATE)
-console.log(process.env.PREVIEW_DEPLOYMENT_TIME)
+console.log(process.env.PREVIEW_DEPLOYMENT_DATE)
 console.log(process.env.PREVIEW_BRANCH_NAME)
 
